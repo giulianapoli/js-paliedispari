dff3c69a6128de39eb57c686e36bb6ee0bcb0cbd
Giulia Napoli <gnapoli1996@gmail.com>
2020-10-27 22:18:33 +0100

    ended ex 2

--- ex-2-pariedispari/script.js
+++ ex-2-pariedispari/script.js
@@ -1,22 +1,34 @@
-var paridispariUtente = prompt("Pari o Dispari?").toLocaleLowerCase();
+var paridispariUtente = prompt("Pari o Dispari?").toLowerCase();
 console.log(paridispariUtente);
-var numeroUtente = prompt("Inserisci un numero da 1 a 5");
+var numeroUtente = parseInt(prompt("Inserisci un numero da 1 a 5"));
 console.log(numeroUtente);
-var somma =  + numeroUtente;
+
+var pcNum = randomNum(1, 5);
+var somma = pcNum + numeroUtente;
 console.log(somma);
-var pcNum = randomNum();
 
 // funzione mathrandom
-function randomNum (min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min)) + min
+function randomNum(min, max) {
+  return (Math.floor(Math.random() * max) + min);
 }
+console.log(randomNum(1, 5));
 
 
 // dichiaro la funzione che verifica numero pari o dispari
-function paridispari() {
-  if (paridispariUtente == "pari") {
-
+function paridispari(num) {
+  if (num % 2 === 0) {
+    var pd = "pari";
+    console.log(pd);
+  } else {
+    var pd = "dispari";
   }
+  return pd;
+}
+console.log(paridispari(somma));
+
+
+if (paridispari(somma) === (paridispariUtente)) {
+  document.getElementById("result").innerHTML = "Hai vinto";
+} else {
+  document.getElementById("result").innerHTML = "Hai perso";
 }
